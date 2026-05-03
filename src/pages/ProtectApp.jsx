78a0bb88cb9448d39842c6e6378954bc9f124429
KosaKasanda75 <kosa.zk@gmail.com>
2026-05-03 22:07:28 +0300

    fixing refrech bug

--- src/pages/ProtectApp.jsx
+++ src/pages/ProtectApp.jsx
@@ -1,9 +1,12 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import FullPageLoading from "./FullPageLoading";
 
 function ProtectApp() {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, authVerifying } = useAuth();
   const location = useLocation();
+
+  if (authVerifying) return <FullPageLoading />;
 
   return isAuthenticated ? (
     <Outlet />
