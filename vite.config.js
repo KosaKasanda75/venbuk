e1ef96d7cd4cbb06c430b61acc4f7fe4f16b391c
KosaKasanda75 <kosa.zk@gmail.com>
2026-05-05 12:40:35 +0300

    Trying to fix deployment bug

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import eslint from "vite-plugin-eslint";
 
 const API_TARGET = "https://api.venbuk.com";
 const proxyRoute = { target: API_TARGET, changeOrigin: true };
+// const proxyRoute = {
+//   target: `http://100.122.206.124:8002`,
+//   changeOrigin: true,
+//   rewrite: (path) => path.replace(/^\/api/, ""),
+// };
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,3 +22,11 @@ export default defineConfig({
     },
   },
 });
+// export default defineConfig({
+//   plugins: [react(), eslint()],
+//   server: {
+//     proxy: {
+//       "/api": proxyRoute,
+//     },
+//   },
+// });
